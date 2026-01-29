1bdce2ba4e104157b040974d9e134b428b46807d
Kannababu.Karri <kkarri@gmail.com>
2026-01-29 12:28:19 -0500

    Added JWT authentication and authorization

--- src/main/java/com/restful/orderqty/controller/OrderQtyController.java
+++ src/main/java/com/restful/orderqty/controller/OrderQtyController.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.restful.orderqty.entity.OrderQty;
 import com.restful.orderqty.exception.InvalidOrderQtyException;
@@ -22,7 +22,7 @@ import com.restful.orderqty.exception.OrderQtyNotFoundException;
 import com.restful.orderqty.exception.ServiceException;
 import com.restful.orderqty.service.OrderQtyService;
 
-@Controller
+@RestController
 @RequestMapping("/api/orderqty")
 public class OrderQtyController {
 	
